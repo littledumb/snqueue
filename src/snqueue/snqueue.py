59cab64d8b1b71e3e116b412c4e94ad48afa9691
littledumb <jiamin@gmail.com>
2023-10-03 10:24:21 +0800

    almost done

--- src/snqueue/snqueue.py
+++ src/snqueue/snqueue.py
@@ -1,10 +1,14 @@
 import boto3
 
-from dataclasses import dataclass
-
-class AwsAgent:
+class Boto3Client:
   """
-  A base class for AWS agents
+  A base class for AWS clients.
+
+  :type client_name: string
+  :param client_name: The name of the boto3 client, such as "sqs", "sns", "s3", etc.
+
+  :type profile_name: string
+  :param profile_name: The AWS profile name
   """
   def __init__(self,
                client_name: str,
@@ -22,42 +26,111 @@ class AwsAgent:
     self.client.close()
     return
 
-@dataclass
-class SqsArgs:
-  MaxNumberOfMessages: int = 1
-  VisibilityTimeout: int = 30
-  WaitTimeSeconds: int = 5
-
-class SqsAgent(AwsAgent):
+class SqsClient(Boto3Client):
   """
-  An agent For retrieving messages from SQS
+  A boto3 client receives messages from SQS.
+
+  :type profile_name: string
+  :param profile_name: The AWS profile name
   """
   def __init__(self,
-               profile_name: str,
-               sqs_url: str,
-               sqs_args: SqsArgs = None):
+               profile_name: str):
     super().__init__("sqs", profile_name)
-    self.sqs_url = sqs_url
-    self.sqs_args = sqs_args or SqsArgs()
     return
   
-  def pull_messages(self, delete: bool) -> list[dict]:
+  def pull_messages(self,
+                    sqs_url: str,
+                    sqs_args: dict) -> list[dict]:
+    """
+    Pull messages from SQS.
+    
+    :type sqs_url: string
+    :param sqs_url: The URL of the SQS queue
+
+    :type sqs_args: dict
+    :param sqs_args: The dict that provides additional arguments (e.g. {"MaxNumberOfMessages": 1})
+    
+    :rtype: list
+    :return: The list of messages retrieved
+    """
+    sqs_args = sqs_args or {}
+
     response = self.client.receive_message(
-      QueueUrl = self.sqs_url,
-      *self.sqs_args
+      QueueUrl = sqs_url,
+      **sqs_args
     )
+
     return response.get('Messages', [])
   
-  def delete_messages(self, messages: list[dict]) -> int:
+  def delete_messages(self,
+                      sqs_url: str,
+                      messages: list[dict]) -> int:
+    """
+    Delete messages from SQS.
+
+    :type sqs_url: string
+    :param sqs_url: The URL of the SQS queue
+
+    :type messages: list
+    :param messages: The list of messages to be deleted
+
+    :rtype: int
+    :return: The number of deleted messages
+    """
     for msg in messages:
       self.client.delete_message(
-        QueueUrl=self.sqs_url,
+        QueueUrl=sqs_url,
         ReceiptHandle=msg['ReceiptHandle']
       )
     return len(messages)
+  
+class SnsClient(Boto3Client):
+  """
+  A boto3 client sends notificaitons to SNS.
+
+  :type profile_name: string
+  :param profile_name: The name of AWS profile
+  """
+  def __init__(self,
+               profile_name: str
+               ):
+    super().__init__("sns", profile_name)
+    return
+  
+  def publish(self,
+              topic_arn: str,
+              message: str,
+              sns_args: dict) -> dict:
+    """
+    Publish message to SNS.
+    
+    :type topic_arn: string
+    :param topic_arn: The ARN of the SNS topic
+
+    :type message: string
+    :param message: The message to be pulished
+
+    :type sns_args: dict
+    :param sns_args: The dict that provides additional arguments (e.g. {"MessageDeduplicationId": "x"})
+
+    :rtype: dict
+    :return: The SNS response of publishing the message
+    """
+    sns_args = sns_args or {}
+
+    return self.client.publish(
+      TopicArn = topic_arn,
+      Message = message,
+      **sns_args
+    )
 
 class SnQueue:
+  """
+  An SNS/SQS event messenger.
 
+  :type profile_name: string
+  :param profile_name: The name of AWS profile
+  """
   def __init__(self,
                profile_name: str):
     self.profile_name = profile_name
@@ -66,16 +139,48 @@ class SnQueue:
   def retrieve(self,
                sqs_url: str,
                delete: bool = True,
-               sqs_args: SqsArgs = None) -> list[dict]:
-    with SqsAgent(self.profile_name,
-                  sqs_url,
-                  sqs_args) as sqs:
-      messages = sqs.pull_messages(delete)
+               sqs_args: dict = None) -> list[dict]:
+    """
+    Retrieve messages.
+
+    :type sqs_url: string
+    :param sqs_url: The URL of the SQS queue
+    
+    :type delete: bool
+    :param delete: Whether to delete messages after receiving them. Default is True.
+
+    :type sqs_args: dict
+    :param sqs_args: The dict that provides additional arguments (e.g. {"MaxNumberOfMessages": 1})
+
+    :rtype: list
+    :return: The list of messages retrieved
+    """
+    with SqsClient(self.profile_name) as sqs:
+      messages = sqs.pull_messages(sqs_url, sqs_args)
 
       if delete:
-        sqs.delete_messages(messages)
-        
+        sqs.delete_messages(sqs_url, messages)
+
       return messages
     
-  def notify(self, sns_topic: str):
-    pass
+  def notify(self,
+             sns_topic_arn: str,
+             message: str,
+             sns_args: dict = None) -> dict:
+    """
+    Send notifications.
+
+    :type sns_topic_arn: string
+    :param sns_topic_arn: The ARN of the SNS topic
+
+    :type message: string
+    :param message: The notification message
+
+    :type sns_args: dict
+    :param sns_args: The dict that provides additional arguments (e.g. {"MessageDeduplicationId": "x"})
+
+    :rtype: dict
+    :return: The SNS response of publishing the message
+    """
+    with SnsClient(self.profile_name) as sns:
+      return sns.publish(sns_topic_arn, message, sns_args)
