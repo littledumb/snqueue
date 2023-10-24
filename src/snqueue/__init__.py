7baa8038a70ba415695b939424787b3c22ba6768
littledumb <jiamin@gmail.com>
2023-10-24 11:25:26 +0800

    add download_from_url utility function

--- src/snqueue/__init__.py
+++ src/snqueue/__init__.py
@@ -1,7 +1,10 @@
 import logging
+import requests
+import shutil
 
 from apscheduler.schedulers.background import BackgroundScheduler
 from snqueue.snqueue import DataModel, ServiceFunc, SnQueueMessenger, SnQueueService
+from urllib.parse import unquote
 
 def start_service(
     service: SnQueueService,
@@ -30,3 +33,28 @@ def start_service(
   service.logger.info('The service `%s` is up and running.' % service.name)
 
   return scheduler
+
+def download_from_url(
+    url: str,
+    local_filename: str=None
+) -> str | None:
+  """
+  Download a file from URL
+
+  :param url: string
+  :param local_filename: string
+  :return: The name of local file downloaded
+  :return: None if error
+  """
+  if not local_filename:
+    local_filename = unquote(url).split('/')[-1].split('?')[0]
+
+  try:
+    with requests.get(url, stream=True) as r:
+      with open(local_filename, 'wb') as f:
+        shutil.copyfileobj(r.raw, f)
+  except Exception as e:
+    logging.error(e)
+    return None
+
+  return local_filename
