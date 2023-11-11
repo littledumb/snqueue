d82341e92b93ec6a21f04a66bffb3bae766ff12c
littledumb <jiamin@gmail.com>
2023-11-11 21:10:06 +0800

    minor modification

--- src/snqueue/utils/file.py
+++ src/snqueue/utils/file.py
@@ -15,31 +15,43 @@ async def aio_download_single(
     silent: bool=True,
     **kwargs
 ) -> str | None:
-  filename = unquote(url).split('/')[-1].split('?')[0]
-  dest = os.path.join(dest_dir, filename)
-
   async with semaphore:
     async with session.get(url, **kwargs) as r:
-      if r.status == 200:
-        async with aiofiles.open(dest, mode="wb") as f:
-          if not silent:
-            print(f"Start downloading {url} into {dest}...")
-          await f.write(await r.read())
-          if not silent:
-            print(f"{url} is downloaded into {dest}.")
-          return dest
-      else:
+      if not r.status == 200:
         return None
+      
+      header = r.headers.get("content-disposition")
+      if header:
+        filename = header.split("filename=")[1]
+      else:
+        filename = unquote(url.split('?')[0].split('/')[-1])
+      dest = os.path.join(dest_dir, filename)
+      
+      async with aiofiles.open(dest, mode="wb") as f:
+        if not silent:
+          print(f"Start downloading {url} into {dest}...")
+        await f.write(await r.read())
+        if not silent:
+          print(f"{url} is downloaded into {dest}.")
+        return dest
 
 async def aio_download(
     session: aiohttp.ClientSession,
     urls: list[str],
     dest_dir: str,
     **kwargs
-) -> str:
+) -> list[str]:
   semaphore = asyncio.Semaphore(MAX_TASKS)
   tasks = map(
     lambda url: aio_download_single(semaphore, session, url, dest_dir, **kwargs),
     urls
   )
   return await asyncio.gather(*tasks)
+
+def download(urls: list[str], dest_dir: str, **kwargs) -> list[str]:
+  async def _async_download():
+    async with aiohttp.ClientSession() as session:
+      dest = await aio_download(session, urls, dest_dir, **kwargs)
+      return dest
+  return asyncio.run(_async_download())
+
